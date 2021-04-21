acb5669874ea6549e3bde0a6a2e9ba1d4d7a5788
joshuajlo <32527323+joshuajlo@users.noreply.github.com>
2021-04-21 17:21:11 -0400

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,23 +5,14 @@ export default function Index() {
     <div className="min-vh-100 d-flex align-items-center p-3 bg-light">
       <div className="card rounded-lg mx-auto border" style={{ maxWidth: '28rem' }}>
         <div className="card-header bg-white py-3">
-          <h4 className="mb-0">Setup</h4>
+          <h4 className="mb-0">Brisk AIO</h4>
         </div>
         <div className="card-body">
         <p>
-          Before you begin, make sure you have followed the completed
-          the <a href="https://github.com/meta-labs/samples/tree/main/custom-checkout#required-configuration" target="_blank">required
-          configuration</a>.
-          You must also disable <a href="https://hyper.co/settings/protection" target="_blank">bot
-          protection</a> and <a href="https://hyper.co/settings/payments" target="_blank">require login</a> for custom
-          checkout to work.
+          It seems that there is no available stock right now. Follow our Twitter @BriskAIO to be notified on our next 
+          planned restock.
         </p>
-        <p>
-          When you're ready, <a href="https://hyper.co/new" target="_blank">create a release</a> in the Hyper dashboard.
-          Once you've created a release, copy the password and go to the following page on this site.
-        </p>
-
-        <div className="bg-light rounded my-1 px-2 py-1 text-monospace">/purchase?password={'<password>'}</div>
+        <div className="bg-light rounded my-1 px-2 py-1 text-monospace"></div>
         </div>
       </div>
     </div>
